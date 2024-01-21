f6fd554462252715c29a8af6fed92f648f47cbe8
CodeByIshika <ishika11518@gmail.com>
2024-01-21 13:11:40 -0600

    Complete music program

--- Program.cs
+++ Program.cs
@@ -16,34 +16,49 @@ namespace lab_05
             {
                 try
                 {
-                    Console.WriteLine("Choose an option: ");
-                    Console.WriteLine("1. Add a song to your playlist.");
-                    Console.WriteLine("2. Play the next song in your playlist.");
-                    Console.WriteLine("3. Skip the next song.");
-                    Console.WriteLine("4. Rewind one song.");
-                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("");
+                    Console.WriteLine(" ---------------------------------------- ");
+                    Console.WriteLine("|          Choose an option:             |");
+                    Console.WriteLine(" ---------------------------------------- ");
+                    Console.WriteLine("  1. Add a song to your playlist.");
+                    Console.WriteLine(" ----------------------------------------");
+                    Console.WriteLine("  2. Play the next song in your playlist.");
+                    Console.WriteLine(" ----------------------------------------");
+                    Console.WriteLine("  3. Skip the next song.");
+                    Console.WriteLine(" ----------------------------------------");
+                    Console.WriteLine("  4. Rewind one song.");
+                    Console.WriteLine(" ---------------------------------------");
+                    Console.WriteLine("  5. Exit");
+                    Console.WriteLine(" ---------------------------------------");
 
-                    Console.Write("Write your choice(1, 2, 3, 4, 5): ");
+                    Console.WriteLine("\n");
+                    Console.Write("Enter your choice(1, 2, 3, 4, 5): ");
                     int userChoice = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
 
                     switch (userChoice)
                     {
                         case 1:
+                            Console.Clear();
                             Console.Write("Enter the song name: ");
                             string songName = Console.ReadLine();
+                            Console.WriteLine("");
                             playlistObj.AddSong(songName);
                             break;
 
                         case 2:
+                            Console.Clear();
                             playlistObj.PlayNextSong();
                             break;
 
 
                         case 3:
+                            Console.Clear();
                             playlistObj.SkipSong();
                             break;
 
                         case 4:
+                            Console.Clear();
                             playlistObj.RewindSong();
                             break;
 
@@ -56,8 +71,14 @@ namespace lab_05
                             break;
                     }
                 }
+                catch (FormatException)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Error: Please enter a valid choice.");
+                }
                 catch (Exception ex)
                 {
+                    Console.Clear();
                     Console.WriteLine($"Error: {ex.Message}");
                 }
             }
--- Songs.cs
+++ Songs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace lab_05
@@ -11,73 +12,124 @@ namespace lab_05
         Queue<string> songsQueue = new Queue<string>();
         Stack<string> songsStack = new Stack<string>();
 
+        /// <summary>
+        /// This method checks  the validity of song name and make it doesn't have
+        /// any special character.
+        /// </summary>
+        /// <param name="songName"></param>
+        /// <returns></returns>
+        public bool isSongNameValid(string songName)
+        {
+            // regex for song name
+            string pattern = @"^[A-Za-z0-9\s]*$";
+            if (!Regex.IsMatch(songName, pattern))
+            {
+                Console.WriteLine("Invalid song name. Please enter a valid song.");
+                return false;
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// This method adds a song to the playlist.If the song name 
+        /// is valid, it is added to the playlist, and a success 
+        /// message is displayed, including the name of the added song. 
+        /// </summary>
+        /// <param name="songName"></param>
         public void AddSong(string songName)
         {
             try
             {
-                songsQueue.Enqueue(songName);
-                Console.Clear();
-                Console.WriteLine($"'{songName}' is added to your playlist.");
-                Console.WriteLine("");
-                DisplayNextSong();
+                // check the validity of song name
+                if (isSongNameValid(songName))
+                {
+                    // add the song to the playlist
+                    songsQueue.Enqueue(songName);
+                    Console.WriteLine($"'{songName}' is added to your playlist.");
+                    Console.WriteLine("");
+
+                    // display the next song in the playlist
+                    DisplayNextSong();
+                    Console.WriteLine("");
+                }
             }
             catch (Exception ex)
             {
                 Console.Clear();
                 Console.WriteLine($"Error in adding song : {ex.Message}");
             }
-
         }
 
+
+        /// <summary>
+        /// This method plays the next song of the playlist.
+        /// </summary>
         public void PlayNextSong()
         {
             try
             {
-                Console.Clear();
+                // checks if the playlist is empty or not
                 if (songsQueue.Count == 0)
                 {
                     Console.WriteLine("Your Playlist is empty. Please add songs to your playlist.");
                 }
 
+                // remove the next song fromthe playlist
                 string currentSong = songsQueue.Dequeue();
+
+                // add the removed song onto the stack
                 songsStack.Push(currentSong);
+
+                // Display the currently playing song
                 Console.WriteLine($"Now playing : {currentSong}");
                 Console.WriteLine("");
+
+                // Display the next song in the playlist
                 DisplayNextSong();
             }
-            catch (Exception ex)
+            catch 
             {
                 Console.Clear();
-                Console.WriteLine($"Error in playing next song: {ex.Message}");
+                Console.WriteLine($"Error: No song availabe to play next.");
             }
         }
 
+
+        /// <summary>
+        /// This method skips the next song of the playlist.
+        /// </summary>
         public void SkipSong()
         {
             try
             {
-                Console.Clear();
+                // Check if the playlist is empty
                 if (songsQueue.Count == 0)
                 {
                     Console.WriteLine("Your Playlist is empty. Please add songs to your playlist.");
                 }
 
+                // Remove the next song and display it as skipped
                 string skippedSong = songsQueue.Dequeue();
                 Console.WriteLine($"Skipped : {skippedSong}");
 
             }
-            catch (Exception ex)
+            catch 
             {
                 Console.Clear();
-                Console.WriteLine($"Error in skipping the next song: {ex.Message}");
+                Console.WriteLine($"Error: There is no song available to skip in your playlist.");
             }
         }
 
+
+        /// <summary>
+        /// Rewind to the previous song of the playlist
+        /// </summary>
         public void RewindSong()
         {
             try
             {
-                Console.Clear();
+                // check if there is a previous song in the playlist
                 if (songsStack.Count == 0)
                 {
                     Console.WriteLine("Sorry, there is no previous song to rewind.");
@@ -86,7 +138,7 @@ namespace lab_05
 
                 string previousSong = songsStack.Pop();
                 songsQueue.Enqueue(previousSong);
-                Console.WriteLine($"Rewinding to {previousSong}");
+                Console.WriteLine($"Rewinding to '{previousSong}'");
             }
             catch (Exception ex)
             {
@@ -95,9 +147,20 @@ namespace lab_05
             }
         }
 
+        
+        /// <summary>
+        /// This method displays the next song of the playlist
+        /// </summary>
         public void DisplayNextSong()
         {
-            Console.WriteLine($"Next song: {songsQueue.Peek()}");
+            if (songsQueue.Count > 0)
+            {
+                Console.WriteLine($"Next song: {songsQueue.Peek()}");
+            }
+            else
+            {
+                Console.WriteLine("No upcoming songs in your playlist.");
+            }
         }
     }
 }
